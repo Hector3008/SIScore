dc71c5e5869cb2942c7276a1b0d1d30ef4d8f791
Hector Hernandez <117788780+Hector3008@users.noreply.github.com>
2026-03-28 23:26:35 -0500

    work on frontend-js linking html to function eventlistener

--- src/public/js/btns/postProformBtn.js
+++ src/public/js/btns/postProformBtn.js
@@ -1,24 +1,53 @@
-      const postProformBtn = document.querySelector("#postProformBtn");
-      
-      const body = {
-        texto1: "texto",
-        texto2: "texto2"
-      }
-      
-      postProformBtn.addEventListener("click", (e) => {
-        e.preventDefault()
-        console.log("postProformBtn here")
-        fetch("/api/proforms/", {
-          method: "post",
-          body: JSON.stringify(body),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        })
-          .then((res) => {
-            console.log("res.status here: ", res.status);
-            return res.text();
-          })
-          .then((data) => console.log("data here: ",data));
-      })
+const postProformBtn = document.querySelector("#postProformBtn");
 
+postProformBtn.addEventListener("click", (e) => {
+  //captura de datos, formulario cliente
+    const canal = document.querySelector("#form-canal-de-venta-field").value;
+    const vendedor = document.querySelector("#form-vendedor-field").value;
+    const pago = document.querySelector("#form-forma-de-pago-field").value;
+    const nombre = document.querySelector("#form-client-field").value;
+    const ruc = document.querySelector("#form-ruc-field").value
+    const telefono = document.querySelector("#form-telefono-field").value;  
+    const email = document.querySelector("#form-email-field").value;  
+    const direccion = document.querySelector("#form-direccion-field").value;
+    const provincia = document.querySelector("#form-provincia-field").value;
+    const ciudad = document.querySelector("#form-ciudad-field").value;
+    const entrega = document.querySelector("#form-Entrega-field").value;
+    const destino = document.querySelector("#form-destino-field").value;
+    const agencia = document.querySelector("#form-agencia-field").value
+    const atencion = document.querySelector("#form-atencion-field").value;
+    const fecha = document.querySelector("#form-fecha-field").value;
+
+  e.preventDefault();
+  const body = {
+    cliente: {
+      nombre,
+      ruc,
+      telefono,
+      email,
+      direccion,
+      provincia,
+      ciudad,
+    },
+    entrega,
+    destino,
+    agencia,
+    atencion,
+    fecha,
+    vendedor,
+    canal,
+    pago
+  };
+
+  fetch("/api/proforms/", {
+    method: "post",
+    body: JSON.stringify(body),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => {
+      console.log("res.status here: ", res.status);
+      return res.text();
+    })
+});
