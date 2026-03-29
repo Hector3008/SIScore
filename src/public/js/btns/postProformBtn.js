211c04586d34012e8e0774226219e99bb005a25d
Hector Hernandez <117788780+Hector3008@users.noreply.github.com>
2026-03-29 13:32:03 -0500

    postProformBtn resetValues()

--- src/public/js/btns/postProformBtn.js
+++ src/public/js/btns/postProformBtn.js
@@ -2,21 +2,21 @@ const postProformBtn = document.querySelector("#postProformBtn");
 
 postProformBtn.addEventListener("click", (e) => {
   //captura de datos, formulario cliente
-    const canal = document.querySelector("#form-canal-de-venta-field").value;
-    const vendedor = document.querySelector("#form-vendedor-field").value;
-    const pago = document.querySelector("#form-forma-de-pago-field").value;
-    const nombre = document.querySelector("#form-client-field").value;
-    const ruc = document.querySelector("#form-ruc-field").value
-    const telefono = document.querySelector("#form-telefono-field").value;  
-    const email = document.querySelector("#form-email-field").value;  
-    const direccion = document.querySelector("#form-direccion-field").value;
-    const provincia = document.querySelector("#form-provincia-field").value;
-    const ciudad = document.querySelector("#form-ciudad-field").value;
-    const entrega = document.querySelector("#form-Entrega-field").value;
-    const destino = document.querySelector("#form-destino-field").value;
-    const agencia = document.querySelector("#form-agencia-field").value
-    const atencion = document.querySelector("#form-atencion-field").value;
-    const fecha = document.querySelector("#form-fecha-field").value;
+  const canal = document.querySelector("#form-canal-de-venta-field").value;
+  const vendedor = document.querySelector("#form-vendedor-field").value;
+  const pago = document.querySelector("#form-forma-de-pago-field").value;
+  const nombre = document.querySelector("#form-client-field").value;
+  const ruc = document.querySelector("#form-ruc-field").value;
+  const telefono = document.querySelector("#form-telefono-field").value;
+  const email = document.querySelector("#form-email-field").value;
+  const direccion = document.querySelector("#form-direccion-field").value;
+  const provincia = document.querySelector("#form-provincia-field").value;
+  const ciudad = document.querySelector("#form-ciudad-field").value;
+  const entrega = document.querySelector("#form-Entrega-field").value;
+  const destino = document.querySelector("#form-destino-field").value;
+  const agencia = document.querySelector("#form-agencia-field").value;
+  const atencion = document.querySelector("#form-atencion-field").value;
+  const fecha = document.querySelector("#form-fecha-field").value;
 
   e.preventDefault();
   const body = {
@@ -36,7 +36,7 @@ postProformBtn.addEventListener("click", (e) => {
     fecha,
     vendedor,
     canal,
-    pago
+    pago,
   };
 
   fetch("/api/proforms/", {
@@ -45,9 +45,29 @@ postProformBtn.addEventListener("click", (e) => {
     headers: {
       "Content-Type": "application/json",
     },
+  }).then((res) => {
+    console.log("res.status here: ", res.status);
+    return res.text();
   })
-    .then((res) => {
-      console.log("res.status here: ", res.status);
-      return res.text();
-    })
-});
+  
+  const reset_values= ()=>{
+    document.querySelector("#form-canal-de-venta-field").value = "";
+    document.querySelector("#form-vendedor-field").value = "";
+    document.querySelector("#form-forma-de-pago-field").value = "";
+    document.querySelector("#form-client-field").value = "";
+    document.querySelector("#form-ruc-field").value = "";
+    document.querySelector("#form-telefono-field").value = "";
+    document.querySelector("#form-email-field").value = "";
+    document.querySelector("#form-direccion-field").value = "";
+    document.querySelector("#form-provincia-field").value = "";
+    document.querySelector("#form-ciudad-field").value = "";
+    document.querySelector("#form-Entrega-field").value = "";
+    document.querySelector("#form-destino-field").value = "";
+    document.querySelector("#form-agencia-field").value = "";
+    document.querySelector("#form-atencion-field").value = "";
+    document.querySelector("#form-fecha-field").value = "";
+
+
+  }
+  reset_values()
+        });
