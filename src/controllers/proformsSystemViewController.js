da0a28442a66726a57c008d619e1a1ff6a4360b0
Hector Hernandez <117788780+Hector3008@users.noreply.github.com>
2026-04-25 21:08:16 -0500

    correccion al agregar proforma: ahora cargo el payload de la consulta GET. Un objeto {doc: {}, ...}. Antes solo enviaba el doc. Ahora podré cargar más información fuera del doc. Informacion como la hora de creacion y otros datos de valor

--- src/controllers/proformsSystemViewController.js
+++ src/controllers/proformsSystemViewController.js
@@ -1,23 +1,15 @@
 import { ProductService, ProformsService } from "../services/services.js";
 
+
 export const proformsSystemViewController = async (req, res) => {
-  const result = await ProductService.getAll(req, res);
-
-  const products = result.payload.filter(
-    (product) => product.status != "deleted",
-  );
-  res.render("proformsSystem.handlebars", { products });
-};
-
-export const proformsSystemViewControllerClaude = async (req, res) => {
   const productRequest = await ProductService.getAll(req, res);
 
   const products = productRequest.payload.filter(
     (product) => product.status != "deleted",
   );
-  const proformRequest = await ProformsService.getAll(req,res)
-  const proforms=proformRequest.payload
-  res.render("proformSystemByClaude.handlebars", { products, proforms });
+  const proformRequest = await ProformsService.getAll(req, res);
+  const proforms = proformRequest.payload;
+  res.render("proformSystem.handlebars", { products, proforms });
 };
 
 
--- src/routers/proformsSystemViewRouter.js
+++ src/routers/proformsSystemViewRouter.js
@@ -1,12 +1,10 @@
 import { Router } from "express";
 import {
   proformsSystemViewController,
-  proformsSystemViewControllerClaude,
 } from "../controllers/proformsSystemViewController.js";
 
 const proformsSystemViewRouter = Router();
 
 proformsSystemViewRouter.get("/", proformsSystemViewController);
-proformsSystemViewRouter.get("/claude", proformsSystemViewControllerClaude);
 
 export default proformsSystemViewRouter;
